e7d5abc221e84b5fb9fa071f9026ed77c167bed4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-02 22:58:20 +0530

    PIHooks: Add a system prop to enable debug logs
    Change-Id: I949b21a5a10c575eede42e61f5f75bdbefd9f161

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -49,7 +49,7 @@ import org.json.JSONObject;
 public class PropImitationHooks {
 
     private static final String TAG = "PropImitationHooks";
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = SystemProperties.getBoolean("debug.pihooks.log", false);
     private static final String PIH_SERVICE_NAME = "pih_manager";
 
     private static final Boolean sDisableGmsProps = SystemProperties.getBoolean(
